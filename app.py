1498fab10a1dec49e1b2f818de0e839b4dcc2d8c
Yagi Yuta <131738276+yagiyuu3@users.noreply.github.com>
2023-07-04 15:59:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ if audio_bytes:
     try:
         audio_data = sr.AudioData(audio_bytes, 44100, 2)
         text = r.recognize_google(audio_data, language='ja-JP')
-        st.write("text")
+        st.write(text)
     except speech_recognition.exceptions.UnknownValueError:
         pass
