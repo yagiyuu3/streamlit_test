f37991d15f957e4de5ef0120db91866c9b0227e9
Yagi Yuta <131738276+yagiyuu3@users.noreply.github.com>
2023-07-04 15:25:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import streamlit as st
 from audio_recorder_streamlit import audio_recorder
+import speech_recognition as sr
+ 
+r = sr.Recognizer()
 
 audio_bytes = audio_recorder()
+
 if audio_bytes:
     audio = r.record(audio_bytes)
     text = r.recognize_google(audio, language='ja-JP')
