72934ba2b8628c90ff8357ce4df55546629b8773
Yagi Yuta <131738276+yagiyuu3@users.noreply.github.com>
2023-07-06 09:12:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 import streamlit as st
 from audio_recorder_streamlit import audio_recorder
 import speech_recognition as sr
+import noisereduce as nr
+import numpy as np
 
 r = sr.Recognizer()
 
 audio_bytes = audio_recorder(pause_threshold=5.0)
 
 if audio_bytes:
-    audio_data = sr.AudioData(audio_bytes, 44100, 2)
+    # Convert audio bytes to numpy array
+    audio_np = np.frombuffer(audio_bytes, np.int16)
+    
+    # Reduce noise
+    reduced_noise = nr.reduce_noise(audio_clip=audio_np, noise_clip=audio_np, verbose=False)
+    
+    # Convert numpy array to audio data
+    audio_data = sr.AudioData(reduced_noise.tobytes(), 44100, 2)
+    
     try:
         text = r.recognize_google(audio_data, language='ja-JP')
         st.write(text)
