5abea4f05d4d20c291afea6a68c9221023fe1747
Yagi Yuta <131738276+yagiyuu3@users.noreply.github.com>
2023-07-06 09:08:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import streamlit as st
 from audio_recorder_streamlit import audio_recorder
 import speech_recognition as sr
- 
+
 r = sr.Recognizer()
 
 audio_bytes = audio_recorder(pause_threshold=5.0)
 
 if audio_bytes:
     audio_data = sr.AudioData(audio_bytes, 44100, 2)
-    text = r.recognize_google(audio_data, language='ja-JP')
-    st.write(text)
+    try:
+        text = r.recognize_google(audio_data, language='ja-JP')
+        st.write(text)
+    except sr.UnknownValueError:
+        st.error("音声を認識できませんでした。もう一度お試しください。")
+    except sr.RequestError as e:
+        st.error(f"Google Speech Recognition APIからエラーが返されました: {e}")
