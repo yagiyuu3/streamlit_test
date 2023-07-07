d1a6b66312eed4360265201a4b82f32d0b26006f
Yagi Yuta <131738276+yagiyuu3@users.noreply.github.com>
2023-07-07 10:31:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,12 @@
 import streamlit as st
 from audio_recorder_streamlit import audio_recorder
-import whisper
-import wave
-
-# モデルの読み込み
-model = whisper.load_model("base")
 
 # レコーディング
 audio_bytes = audio_recorder(pause_threshold=5.0)
 
+def voice_to_text():
+    transcript = openai.Audio.transcribe('whisper-1', audio_bytes)
+    return transcript['text']
+    
 if audio_bytes:
-    # 音声データを一時的な音声ファイルに保存
-    with wave.open("temp.wav", "wb") as f:
-        f.setnchannels(1)
-        f.setsampwidth(2)
-        f.setframerate(44100)
-        f.writeframes(audio_bytes)
-
-    # 音声ファイルの文字起こし
-    result = model.transcribe("temp.wav")
-
-    # 結果の表示
-    st.write(result["text"])
+    st.write(voice_to_text())
