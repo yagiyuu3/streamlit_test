cef5e83a44b5baee5a7121c579d99576a58b6a0e
Yagi Yuta <131738276+yagiyuu3@users.noreply.github.com>
2023-07-07 10:32:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 from audio_recorder_streamlit import audio_recorder
+import openai
+
+# Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
+openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
 # レコーディング
 audio_bytes = audio_recorder(pause_threshold=5.0)
