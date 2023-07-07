df2399e644f37ad622951758111f47dc96f010cd
Yagi Yuta <131738276+yagiyuu3@users.noreply.github.com>
2023-07-07 10:10:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 import streamlit as st
 from audio_recorder_streamlit import audio_recorder
-import speech_recognition as sr
+import whisper
+    
+# モデルの読み込み
+model = whisper.load_model("base")
 
+# レコーディング
 r = sr.Recognizer()
 
 audio_bytes = audio_recorder(pause_threshold=5.0)
 
 if audio_bytes:
     audio_data = sr.AudioData(audio_bytes, 44100, 2)
-    try:
-        text = r.recognize_google(audio_data, language='ja-JP')
-        st.write(text)
-    except sr.UnknownValueError:
-        st.error("音声を認識できませんでした。もう一度お試しください。")
-    except sr.RequestError as e:
-        st.error(f"Google Speech Recognition APIからエラーが返されました: {e}")
+
+    # 音声ファイルの文字起こし
+    result = model.transcribe(audio_data)
+
+    # 結果の表示
+    st.write(result["text"])
