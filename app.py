10f7e3de6b526db10790e2b989fabde5e54d4713
Yagi Yuta <131738276+yagiyuu3@users.noreply.github.com>
2023-07-07 10:36:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,14 @@ openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 audio_bytes = audio_recorder(pause_threshold=5.0)
 
 def voice_to_text():
-    transcript = openai.Audio.transcribe('whisper-1', audio_bytes)
+    # 音声データを一時的な音声ファイルに保存
+    with open("temp.wav", "wb") as f:
+        f.write(audio_bytes)
+
+    # 音声ファイルの文字起こし
+    with open("temp.wav", "rb") as f:
+        transcript = openai.Audio.transcribe('whisper-1', f)
+    
     return transcript['text']
     
 if audio_bytes:
