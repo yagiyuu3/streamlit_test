7e53d5a60d868222cd6cba00219d3f5b736bcd3d
Yagi Yuta <131738276+yagiyuu3@users.noreply.github.com>
2023-07-10 12:17:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,33 @@ import streamlit as st
 import streamlit.components.v1 as stc
 import base64
 import time
+from gtts import gTTS 
 
 button = st.button('アプリ実行')
+
 if button:
-    audio_path1 = 'sample.wav' #入力する音声ファイル
+
+
+    myText = "おはようございます"
+    language ='ja'
+    output = gTTS(text=myText, lang=language, slow=False)
+    output.save("output.mp3")
+    audio_path1 = 'output.mp3' #入力する音声ファイル
+
     audio_placeholder = st.empty()
+
     file_ = open(audio_path1, "rb")
     contents = file_.read()
     file_.close()
+
     audio_str = "data:audio/ogg;base64,%s"%(base64.b64encode(contents).decode())
-    audio_html = """<audio autoplay=True><source src="%s" type="audio/ogg" autoplay=True>Your browser does not support the audio element.</audio>""" %audio_str
+    audio_html = """
+                    <audio autoplay=True>
+                    <source src="%s" type="audio/ogg" autoplay=True>
+                    Your browser does not support the audio element.
+                    </audio>
+                """ %audio_str
+
     audio_placeholder.empty()
     time.sleep(0.5) #これがないと上手く再生されません
-    audio_placeholder.markdown(audio_html, unsafe_allow_html=True
+    audio_placeholder.markdown(audio_html, unsafe_allow_html=True)
