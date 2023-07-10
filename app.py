36bfea90d13cbf49b85a24154c685f52ec42d46b
Yagi Yuta <131738276+yagiyuu3@users.noreply.github.com>
2023-07-10 11:49:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
 import streamlit as st
-from audio_recorder_streamlit import audio_recorder
-import openai
+import pyttsx3
+import tempfile
 
-# Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
-openai.api_key = st.secrets.OpenAIAPI.openai_api_key
+st.set_page_config(page_title="Text-to-Speech App", page_icon=":microphone:")
 
-# レコーディング
-audio_bytes = audio_recorder(pause_threshold=5.0)
+st.title("Text-to-Speech App")
 
-def voice_to_text():
-    # 音声データを一時的な音声ファイルに保存
-    with open("temp.wav", "wb") as f:
-        f.write(audio_bytes)
+input_text = st.text_area("Enter Text Here")
+voice = st.selectbox("Select Voice", ["en", "ja"])
 
-    # 音声ファイルの文字起こし
-    with open("temp.wav", "rb") as f:
-        transcript = openai.Audio.transcribe('whisper-1', f)
-    
-    return transcript['text']
-    
-if audio_bytes:
-    st.write(voice_to_text())
+if st.button("Speak"):
+    if input_text:
+        engine = pyttsx3.init()
+        engine.setProperty('voice', voice)
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as f:
+            fname = f.name
+            engine.save_to_file(input_text, fname)
+            engine.runAndWait()
+            st.audio(fname, format="audio/wav")
+    else:
+        st.warning("Please enter some text.")
